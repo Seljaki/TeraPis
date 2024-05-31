2ea7bf6fab2c9d0c52a7e17f95d7622a4df9dfb1
MrDog210 <tusakf@gmail.com>
2024-05-31 16:35:10 +0200

    SCRUM-107 fixed bnf

--- src/main/kotlin/SyntaxAnalyzer.kt
+++ src/main/kotlin/SyntaxAnalyzer.kt
@@ -27,14 +27,23 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
     }
 
     fun Statement(): Boolean {
-        if(PlotDefinition()) {
-            return true
+        if (currentToken.symbol == Symbol.PLOT) {
+            nextToken()
+            if(PlotDefinition()) {
+                return true
+            }
         }
-        if(WorkDefinition()) {
-            return true
+        if (currentToken.symbol == Symbol.WORK) {
+            nextToken()
+            if(WorkDefinition()) {
+                return true
+            }
         }
-        if(If()) {
-            return true
+        if (currentToken.symbol == Symbol.IF) {
+            nextToken()
+            if(If()) {
+                return true
+            }
         }
         if (Function()) {
             return true
@@ -43,23 +52,19 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
     }
 
     fun PlotDefinition(): Boolean {
-        if (currentToken.symbol == Symbol.PLOT) {
+        if (currentToken.symbol == Symbol.NAME) {
             nextToken()
-            if (currentToken.symbol == Symbol.NAME) {
+            if (currentToken.symbol == Symbol.LCURLY) {
                 nextToken()
-                if (currentToken.symbol == Symbol.LCURLY) {
-                    nextToken()
-                    if(PlotBody()) {
-                        if (currentToken.symbol == Symbol.RCURLY) {
-                            nextToken()
-                            return true
-                        }
+                if(PlotBody()) {
+                    if (currentToken.symbol == Symbol.RCURLY) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun PlotBody(): Boolean {
@@ -70,37 +75,39 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             return true
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun PlotBody2(): Boolean {
-        if (Coordinates()) {
-            return true
+        if (currentToken.symbol == Symbol.COORDINATES) {
+            nextToken()
+            if (Coordinates()) {
+                return true
+            }
         }
-        if(PlotType()) {
-            return true
+        if (currentToken.symbol == Symbol.TYPE) {
+            nextToken()
+            if(PlotType()) {
+                return true
+            }
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Coordinates(): Boolean {
-        if (currentToken.symbol == Symbol.COORDINATES) {
+        if (currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if (currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.LSQUARE) {
                 nextToken()
-                if(currentToken.symbol == Symbol.LSQUARE) {
-                    nextToken()
-                    if(Points()) {
-                        if(currentToken.symbol == Symbol.RSQUARE) {
-                            nextToken()
-                            return true
-                        }
+                if(Points()) {
+                    if(currentToken.symbol == Symbol.RSQUARE) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Points(): Boolean {
@@ -111,7 +118,7 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             return true
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Point(): Boolean {
@@ -133,53 +140,39 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun PlotType(): Boolean {
-        if(currentToken.symbol == Symbol.TYPE) {
+        if(currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if(currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.FOREST) {
                 nextToken()
-                return PlotType2()
+                return true
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
-        }
-        return false
-    }
-
-    fun PlotType2(): Boolean {
-        if(currentToken.symbol == Symbol.FOREST) {
-            nextToken()
-            return true
-        }
-        if(currentToken.symbol == Symbol.FIELD) {
-            nextToken()
-            return true
+            if(currentToken.symbol == Symbol.FIELD) {
+                nextToken()
+                return true
+            }
         }
         throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun WorkDefinition(): Boolean {
-        if(currentToken.symbol == Symbol.WORK) {
+        if(currentToken.symbol == Symbol.NAME) {
             nextToken()
-            if(currentToken.symbol == Symbol.NAME) {
+            if (currentToken.symbol == Symbol.LCURLY) {
                 nextToken()
-                if (currentToken.symbol == Symbol.LCURLY) {
-                    nextToken()
-                    if(WorkBody()) {
-                        if (currentToken.symbol == Symbol.RCURLY) {
-                            nextToken()
-                            return true
-                        }
+                if(WorkBody()) {
+                    if (currentToken.symbol == Symbol.RCURLY) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun WorkBody(): Boolean {
@@ -190,46 +183,57 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             return true
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun WorkBody2(): Boolean {
-        if (Path()) {
-            return true
+        if(currentToken.symbol == Symbol.PATH) {
+            nextToken()
+            if (Path()) {
+                return true
+            }
         }
-        if (Action()) {
-            return true
+        if(currentToken.symbol == Symbol.ACTION) {
+            nextToken()
+            if (Action()) {
+                return true
+            }
         }
-        if (MaxSpeed()) {
-            return true
+        if(currentToken.symbol == Symbol.MAX_SPEED) {
+            nextToken()
+            if (MaxSpeed()) {
+                return true
+            }
         }
-        if(ImplementWidth()) {
-            return true
+        if(currentToken.symbol == Symbol.IMPLEMENT_WIDTH) {
+            nextToken()
+            if (ImplementWidth()) {
+                return true
+            }
         }
-        if (WorkPlot()) {
-            return true
+        if(currentToken.symbol == Symbol.PLOT) {
+            nextToken()
+            if (WorkPlot()) {
+                return true
+            }
         }
         return false
     }
 
     fun Path(): Boolean {
-        if(currentToken.symbol == Symbol.PATH) {
+        if(currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if(currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.LSQUARE) {
                 nextToken()
-                if(currentToken.symbol == Symbol.LSQUARE) {
-                    nextToken()
-                    if(Pointts()) {
-                        if(currentToken.symbol == Symbol.RSQUARE) {
-                            nextToken()
-                            return true
-                        }
+                if(Pointts()) {
+                    if(currentToken.symbol == Symbol.RSQUARE) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Pointts(): Boolean {
@@ -240,7 +244,7 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             return true
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Pointt(): Boolean {
@@ -268,156 +272,114 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Action(): Boolean {
-        if (currentToken.symbol == Symbol.ACTION) {
+        if (currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if (currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.NAME) {
                 nextToken()
-                if(currentToken.symbol == Symbol.NAME) {
-                    nextToken()
-                    return true
-                }
+                return true
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun MaxSpeed(): Boolean {
-        if (currentToken.symbol == Symbol.MAX_SPEED) {
+        if (currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if (currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.REAL) {
                 nextToken()
-                if(currentToken.symbol == Symbol.REAL) {
-                    nextToken()
-                    return true
-                }
+                return true
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun ImplementWidth(): Boolean {
-        if (currentToken.symbol == Symbol.IMPLEMENT_WIDTH) {
+        if (currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if (currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.REAL) {
                 nextToken()
-                if(currentToken.symbol == Symbol.REAL) {
-                    nextToken()
-                    return true
-                }
+                return true
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun WorkPlot(): Boolean {
-        if (currentToken.symbol == Symbol.PLOT) {
+        if (currentToken.symbol == Symbol.COLON) {
             nextToken()
-            if (currentToken.symbol == Symbol.COLON) {
+            if(currentToken.symbol == Symbol.NAME) {
                 nextToken()
-                if(currentToken.symbol == Symbol.NAME) {
-                    nextToken()
-                    return true
-                }
+                return true
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun If(): Boolean {
-        if (currentToken.symbol == Symbol.IF) {
+        if (currentToken.symbol == Symbol.PLOT) {
             nextToken()
-            if (currentToken.symbol == Symbol.PLOT) {
+            if (currentToken.symbol == Symbol.NAME) {
                 nextToken()
-                if (currentToken.symbol == Symbol.NAME) {
-                    nextToken()
-                    return If2()
-                }
+                return If2()
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun If2(): Boolean {
-        if(IfIsValid()) {
-            return true
+        if (currentToken.symbol == Symbol.IS) {
+            nextToken()
+            if(IfIsValid()) {
+                return true
+            }
         }
-        if (IfContains()) {
-            return true
+        if (currentToken.symbol == Symbol.CONTAINS) {
+            nextToken()
+            if (IfContains()) {
+                return true
+            }
         }
         throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun IfIsValid(): Boolean {
-        if (currentToken.symbol == Symbol.IS) {
+        if (currentToken.symbol == Symbol.VALID) {
             nextToken()
-            if (currentToken.symbol == Symbol.VALID) {
+            if(currentToken.symbol == Symbol.LCURLY) {
                 nextToken()
-                if(currentToken.symbol == Symbol.LCURLY) {
-                    nextToken()
-                    if (Statements()) {
-                        if(currentToken.symbol == Symbol.RCURLY) {
-                            nextToken()
-                            return true
-                        }
+                if (Statements()) {
+                    if(currentToken.symbol == Symbol.RCURLY) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun IfContains(): Boolean {
-        if (currentToken.symbol == Symbol.CONTAINS) {
+        if (currentToken.symbol == Symbol.NAME) {
             nextToken()
-            if (currentToken.symbol == Symbol.NAME) {
+            if(currentToken.symbol == Symbol.LCURLY) {
                 nextToken()
-                if(currentToken.symbol == Symbol.LCURLY) {
-                    nextToken()
-                    if (Statements()) {
-                        if(currentToken.symbol == Symbol.RCURLY) {
-                            nextToken()
-                            return true
-                        }
+                if (Statements()) {
+                    if(currentToken.symbol == Symbol.RCURLY) {
+                        nextToken()
+                        return true
                     }
                 }
             }
-            throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
+        throw IllegalStateException("Encountered invalid token: $currentToken")
     }
 
     fun Function(): Boolean {
-        if (CalculatePath()) {
-            return true
-        }
-        if (CalculateArea()) {
-            return true
-        }
-        if (CalculateAreaCovered()) {
-            return true
-        }
-        if (CalculateAverageSpeed()) {
-            return true
-        }
-        if (CalculateEfficiency()) {
-            return true
-        }
-        return false
-    }
-
-    fun CalculatePath(): Boolean {
         if (currentToken.symbol == Symbol.CALCULATE_PATH) {
             nextToken()
             if (currentToken.symbol == Symbol.LPAREN) {
@@ -438,10 +400,6 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
-    }
-
-    fun CalculateArea(): Boolean {
         if (currentToken.symbol == Symbol.CALCULATE_AREA) {
             nextToken()
             if (currentToken.symbol == Symbol.LPAREN) {
@@ -456,10 +414,6 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
-    }
-
-    fun CalculateAreaCovered(): Boolean {
         if (currentToken.symbol == Symbol.CALCULATE_AREA_COVERED) {
             nextToken()
             if (currentToken.symbol == Symbol.LPAREN) {
@@ -474,10 +428,6 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
-    }
-
-    fun CalculateAverageSpeed(): Boolean {
         if (currentToken.symbol == Symbol.CALCULATE_AVERAGE_SPEED) {
             nextToken()
             if (currentToken.symbol == Symbol.LPAREN) {
@@ -492,10 +442,6 @@ class SyntaxAnalyzer(private val scanner: Scanner) {
             }
             throw IllegalStateException("Encountered invalid token: $currentToken")
         }
-        return false
-    }
-
-    fun CalculateEfficiency(): Boolean {
         if (currentToken.symbol == Symbol.CALCULATE_EFFICIENCY) {
             nextToken()
             if (currentToken.symbol == Symbol.LPAREN) {
