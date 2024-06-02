2c02f22ebe4eab851d03dce957099fd12f277a8a
MrDog210 <tusakf@gmail.com>
2024-06-02 22:30:24 +0200

    Revert changes

--- src/test/kotlin/tests/SemanticAnalyzerTest.kt
+++ src/test/kotlin/tests/SemanticAnalyzerTest.kt
@@ -24,7 +24,7 @@ class SemanticAnalyzerTest {
             }
             println(result)
             assertTrue(true)
-            //assertTrue(result)
+            assertTrue(result)
         }
     }
 }
