911b8e15919251699bd857dd94a22456f70d9a52
MrDog210 <tusakf@gmail.com>
2024-06-13 10:07:42 +0200

    SCRUM-63 updated tests

--- src/test/kotlin/tests/SemanticAnalyzerTest.kt
+++ src/test/kotlin/tests/SemanticAnalyzerTest.kt
@@ -10,7 +10,7 @@ import java.io.File
 class SemanticAnalyzerTest {
     @Test
     fun testGood() {
-        val dir = File("semantika_tests")
+        val dir = File("semantika_tests/good")
         val files = dir.listFiles()
         for (file in files) {
             println("\nTesting file ${file.name}")
@@ -24,6 +24,25 @@ class SemanticAnalyzerTest {
             }
             println(result)
             assertTrue(result)
+        }
+
+        @Test
+        fun testBad() {
+            val dir = File("semantika_tests/bad")
+            val files = dir.listFiles()
+            for (file in files) {
+                println("\nTesting file ${file.name}")
+                var result = false
+                try {
+                    result = SemanticAnalyzer(Scanner(Lexicon, file.inputStream())).parse()
+                    println("No error")
+                } catch (e: Exception) {
+                    println("Error")
+                    println(e)
+                }
+                println(result)
+                assertFalse(result)
+            }
         }
     }
 }
